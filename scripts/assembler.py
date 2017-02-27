ade2e59cdc1943c2d48f9419d196193c35d726dc
Frederick Hornsey <fred@hornsey.us>
2017-02-27 16:30:21 -0600

    Added prototype assembler

---
+++ scripts/assembler.py
@@ -0,0 +1,41 @@
+import sys
+from pathlib import Path
+
+NUMBER_OF_ARGUMENTS = 3
+
+if len(sys.argv) != 3:
+    sys.exit('usage: {} INPUT_PATH OUTPUT_PATH'.format(sys.argv[0]))
+
+input_path = Path(sys.argv[1])
+output_path = Path(sys.argv[2])
+
+ops = {
+    "halt": 0,
+    "nop":  1,
+    "set":  2,
+    "copy": 3,
+    "addr": 4,
+    "addv": 5,
+    "subr": 6,
+    "subv": 7,
+}
+
+image = []
+
+for lineno, line in enumerate(input_path.read_text().split('\n')):
+    if line:
+        print("{}: \"{}\"".format(lineno, line))
+        parts = line.split()
+        op = ops[parts[0]]
+        instruction = [0, op]
+        arguments = [list(i) for i in parts[1:]]
+        for i in range(0, NUMBER_OF_ARGUMENTS):
+            if i < len(arguments):
+                instruction.extend([int(n, 16) for n in arguments[i]])
+            else:
+                instruction.extend([0,0])
+        print('   ', instruction)
+        image.extend(instruction)
+
+output_path.write_bytes(bytes(image))
+
